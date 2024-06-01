b6ca308970e12c865ea1c2660b0e7652620ef7fc
Bernhard Schönberger <bernhard.schoenberger@leebmann.de>
2024-06-01 13:33:43 +0200

    add analytics.js

---
+++ docs/js/analytics.js
@@ -0,0 +1,10 @@
+
+if (location.host === "noscrape.github.io") {
+    document.write(`
+        <script 
+            defer 
+            src="https://analytics.schoenberger.be/script.js" 
+            data-website-id="220756d9-429a-4b8b-81d4-9270d80364c4"
+        ></script>
+    `)
+}
